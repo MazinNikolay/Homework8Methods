fcd3f2fdd02e9495a169580ff69d8b882b32f2f5
Nikolay <koliamba_5@mail.ru>
2023-11-25 22:26:30 +0800

    Corrected with recommendation

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,14 @@ public class Main {
         int clientDeviceYear = 2021;
         int clientOs = 1;
         checkOsVersion(clientDeviceYear, clientOs);
-        int deliveryDistance = 60;
+        int deliveryDistance = 0;
         System.out.println();
-        checkDeliveryDistance(deliveryDistance);
-        System.out.println();
+        int deliveryTime = checkDeliveryDistance(deliveryDistance);
+        if (deliveryTime > 0) {
+            System.out.println("Потребуется дней: "+ deliveryTime);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
         int[] array = {3, 2, 1, 6, 5};
         reverseArray(array);
         System.out.println("Измененный массив: " + Arrays.toString(array));
@@ -26,7 +30,9 @@ public class Main {
     public static void isLeapYear(int year) {
         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0 && year % 4 == 0)) {
             System.out.println(year + " год — високосный год");
-        } else System.out.println(year + " год — невисокосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
     }
 
     public static void checkOsVersion(int clientDeviceYear, int clientOs) {
@@ -36,16 +42,18 @@ public class Main {
         System.out.printf("Установите %s версию приложения для %s по ссылке", version, os);
     }
 
-    public static void checkDeliveryDistance(int deliveryDistance) {
-        String deliveryTime = "";
+    public static int checkDeliveryDistance(int deliveryDistance) {
+        int deliveryTime;
         if (deliveryDistance < 20) {
-            deliveryTime = "1";
+            deliveryTime = 1;
         } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            deliveryTime = "2";
+            deliveryTime = 2;
         } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            deliveryTime = "3";
-        } else deliveryTime = "доставки нет";
-        System.out.printf("Потребуется дней: %s", deliveryTime);
+            deliveryTime = 3;
+        } else {
+            deliveryTime = -1;
+        }
+        return deliveryTime;
     }
 
     public static void reverseArray(int[] array) {
@@ -63,11 +71,11 @@ public class Main {
         System.out.println("Строка: " + line);
         int i = 0;
         boolean doubleFound = false;
-        while (i < line.length() && (!doubleFound)) {
-            i++;
-            if (line.charAt(i) == line.charAt(i - 1)) {
+        while (i < line.length() - 1 && (!doubleFound)) {
+            if (line.charAt(i) == line.charAt(i + 1)) {
                 doubleFound = true;
             }
+            i++;
         }
         if (doubleFound) {
             System.out.println("Дубль найден. Дублирующийся символ - " + line.charAt(i));
